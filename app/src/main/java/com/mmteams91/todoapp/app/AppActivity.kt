40e05432f523344ad9757bd0f0feecff0bef9b76
mmarukhin <mmteams91java@gmail.com>
2018-12-26 08:00:20 +0300

    move base events to app activity

--- app/src/main/java/com/mmteams91/todoapp/app/AppActivity.kt
+++ app/src/main/java/com/mmteams91/todoapp/app/AppActivity.kt
@@ -2,13 +2,19 @@ package com.mmteams91.todoapp.app
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.mmteams91.todoapp.R
 import com.mmteams91.todoapp.core.presentation.IDisposableBinder
 import com.mmteams91.todoapp.core.presentation.view.DisposableBinder
 import com.mmteams91.todoapp.core.presentation.viewmodel.BaseViewModel
+import com.mmteams91.todoapp.core.presentation.viewmodel.BaseViewModel.Events.EVENT_ERROR
+import com.mmteams91.todoapp.core.presentation.viewmodel.BaseViewModel.Events.EVENT_HIDE_PROGRESS
+import com.mmteams91.todoapp.core.presentation.viewmodel.BaseViewModel.Events.EVENT_SHOW_PROGRESS
 import com.mmteams91.todoapp.core.presentation.viewmodel.Event
+import com.mmteams91.todoapp.core.presentation.viewmodel.EventWithPayload
 import com.mmteams91.todoapp.core.presentation.viewmodel.ViewModelFactory
 import com.mmteams91.todoapp.features.auth.AuthFragment
 import kotlinx.android.synthetic.main.app_activity.*
@@ -41,7 +47,26 @@ class AppActivity : AppCompatActivity(), com.mmteams91.todoapp.app.IAppView {
     }
 
     override fun obtainEvent(event: Event) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when (event.name) {
+            EVENT_SHOW_PROGRESS -> showProgress()
+            EVENT_HIDE_PROGRESS -> hideProgress()
+            EVENT_ERROR -> showError(event as? EventWithPayload ?: return)
+        }
+    }
+
+    private fun showError(event: EventWithPayload) {
+        val message = if (event.payload is Int) getString(event.payload) else event.payload.toString()
+        val snackbar = Snackbar.make(snackBarContainer ?: return, message, Snackbar.LENGTH_LONG)
+        val view = snackbar.view
+        val context = view.context
+        view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorError))
+        snackbar.setActionTextColor(ContextCompat.getColor(context, android.R.color.white))
+        snackbar.setAction(android.R.string.ok) {
+            if (snackbar.isShown) {
+                snackbar.dismiss()
+            }
+        }
+        snackbar.show()
     }
 
     override fun hideProgress() {
--- app/src/main/java/com/mmteams91/todoapp/core/presentation/view/IView.kt
+++ app/src/main/java/com/mmteams91/todoapp/core/presentation/view/IView.kt
@@ -1,12 +1,13 @@
 package com.mmteams91.todoapp.core.presentation.view
 
-import android.support.annotation.StringRes
 import android.view.View
 import com.mmteams91.todoapp.core.extensions.safeSubscribe
 import com.mmteams91.todoapp.core.presentation.IDisposableBinder
 import com.mmteams91.todoapp.core.presentation.viewmodel.Event
 import com.mmteams91.todoapp.core.presentation.viewmodel.IViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 interface IView<Vm : IViewModel> {
     val snackBarContainer: View?
@@ -15,16 +16,21 @@ interface IView<Vm : IViewModel> {
 
 
     fun onStart() {
-        viewModel.eventsFlow().safeSubscribe{
-            obtainEvent(it)
-        }.also { bind(it) }
+        subscribeToEvents()
         viewModel.onStart()
+    }
+
+    fun subscribeToEvents() {
+        viewModel.eventsFlow()
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .safeSubscribe({ subscribeToEvents() }) {
+                    obtainEvent(it)
+                }.also { bind(it) }
     }
 
     fun obtainEvent(event: Event)
 
-
     fun bind(disposable: Disposable) = disposableBinder.bindToLifecycle(disposable)
 
-    fun getString(@StringRes res: Int): String
 }
